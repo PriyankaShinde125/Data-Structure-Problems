54509e7a219e947f690ba7ea8421d2ba652f583a
PriyankaShinde125 <pkshinde125@gmail.com>
2023-01-02 09:41:15 +0530

    ADDED : Code to read and write to file using ordered list

--- src/main/java/org/example/linkedlist/DataStructureMain.java
+++ src/main/java/org/example/linkedlist/DataStructureMain.java
@@ -12,8 +12,8 @@ import java.util.stream.Stream;
 
 public class DataStructureMain {
     public static final int UNORDERED_LIST = 1;
+    public static final int ORDERED_LIST = 2;
     public static final int EXIT = 0;
-    private MyLinkedList<String> list;
 
     public static void main(String[] args) throws CustomException {
         DataStructureMain dataStructureMain = new DataStructureMain();
@@ -21,11 +21,15 @@ public class DataStructureMain {
         while (true) {
             System.out.println("Enter your choice :" +
                     "\n1 : unordered list operation" +
+                    "\n2 : Ordered list operations" +
                     "\n0 : Exit");
             int choice = sc.nextInt();
             switch (choice) {
                 case UNORDERED_LIST:
                     dataStructureMain.unorderedListReadWriteToFile();
+                    break;
+                case ORDERED_LIST:
+                    dataStructureMain.orderedListReadWriteToFile();
                     break;
                 case EXIT:
                     return;
@@ -36,24 +40,38 @@ public class DataStructureMain {
         }
     }
 
-    private void unorderedListReadWriteToFile() throws CustomException {
-        list = new MyLinkedList<>();
-        String[] strArr = readDataFromFile();
-        Arrays.stream(strArr).forEach(s -> list.add(new MyNode<>(s)));
+    private void orderedListReadWriteToFile() throws CustomException {
+        MyLinkedList<Integer> list = new MyOrderedList<>();
+        String[] strArr = readDataFromFile("orderedlistdata.txt");
+        Arrays.stream(strArr).forEach(s -> list.add(new MyNode<>(Integer.parseInt(s))));
         Scanner sc = new Scanner(System.in);
-        String key = sc.next();
-        if (list.search(new MyNode<>(key)) != null)
-            list.delete(new MyNode<>(key));
+        Integer key = sc.nextInt();
+        if (list.search(new MyNode(key)) != null)
+            list.delete(new MyNode(key));
         else
-            list.add(new MyNode<>(key));
+            list.add(new MyNode(key));
         System.out.println(list);
-        writeDataToTextFile();
+        writeDataToTextFile("orderedlistdata.txt",list);
     }
 
-    String[] readDataFromFile() throws CustomException {
+    private void unorderedListReadWriteToFile() throws CustomException {
+        MyLinkedList<String> list = new MyLinkedList<>();
+        String[] strArr = readDataFromFile("listdata.txt");
+        Arrays.stream(strArr).forEach(s -> list.add(new MyNode(s)));
+        Scanner sc = new Scanner(System.in);
+        String key = sc.next();
+        if (list.search(new MyNode(key)) != null)
+            list.delete(new MyNode(key));
+        else
+            list.add(new MyNode(key));
+        System.out.println(list);
+        writeDataToTextFile("listdata.txt",list);
+    }
+
+    String[] readDataFromFile(String fileName) throws CustomException {
         String[] wordList;
         try {
-            Path path = Paths.get("src/main/resources/listdata.txt");
+            Path path = Paths.get("src/main/resources/" + fileName);
             Stream<String> lines = Files.lines(path);
             String data = lines.collect(Collectors.joining("\\s"));
             wordList = data.toLowerCase().split("\\s");
@@ -64,10 +82,10 @@ public class DataStructureMain {
         return wordList;
     }
 
-    public void writeDataToTextFile() throws CustomException {
-        Path path = Paths.get("src/main/resources/listdata.txt");
+    public <T extends Comparable<T>>void writeDataToTextFile(String fileName,MyLinkedList<T> list) throws CustomException {
+        Path path = Paths.get("src/main/resources/" + fileName);
         try (BufferedWriter bw = Files.newBufferedWriter(path)) {
-            bw.write(list.toString().replaceAll("->"," "));
+            bw.write(list.toString().replaceAll("->", " "));
             System.out.println("Data write to file successfully");
         } catch (IOException e) {
             throw new CustomException(ExceptionType.IO_EXCEPTION);
