de83bd1e459986179c21e42602116e5382e7576f
PriyankaShinde125 <pkshinde125@gmail.com>
2023-01-02 10:10:17 +0530

    ADDED : Code to check simple balanced parenthesis

--- src/main/java/org/example/linkedlist/DataStructureMain.java
+++ src/main/java/org/example/linkedlist/DataStructureMain.java
@@ -13,6 +13,7 @@ import java.util.stream.Stream;
 public class DataStructureMain {
     public static final int UNORDERED_LIST = 1;
     public static final int ORDERED_LIST = 2;
+    public static final int BALANCED_PARENTHESIS = 3;
     public static final int EXIT = 0;
 
     public static void main(String[] args) throws CustomException {
@@ -22,6 +23,7 @@ public class DataStructureMain {
             System.out.println("Enter your choice :" +
                     "\n1 : unordered list operation" +
                     "\n2 : Ordered list operations" +
+                    "\n3 : Balanced parenthesis" +
                     "\n0 : Exit");
             int choice = sc.nextInt();
             switch (choice) {
@@ -30,6 +32,9 @@ public class DataStructureMain {
                     break;
                 case ORDERED_LIST:
                     dataStructureMain.orderedListReadWriteToFile();
+                    break;
+                case BALANCED_PARENTHESIS:
+                    dataStructureMain.ensureBalancedParenthesis();
                     break;
                 case EXIT:
                     return;
@@ -40,10 +45,35 @@ public class DataStructureMain {
         }
     }
 
+    private void ensureBalancedParenthesis() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an expression : ");
+        String expression = sc.next();
+        char[] expressionChars = expression.toCharArray();
+        MyStack myStack = new MyStack<>();
+        for (int i = 0; i < expressionChars.length; i++) {
+            if (expressionChars[i] == '(')
+                myStack.push(new MyNode(expressionChars[0]));
+            if (expressionChars[i] == ')'){
+                if(myStack.isEmpty()){
+                    System.out.println("Expression is not balanced");
+                    return;
+                }
+                myStack.pop();
+            }
+        }
+        if(myStack.isEmpty())
+            System.out.println("Expression is balanced");
+        else
+            System.out.println("Expression is not balanced");
+    }
+
     private void orderedListReadWriteToFile() throws CustomException {
         MyLinkedList<Integer> list = new MyOrderedList<>();
         String[] strArr = readDataFromFile("orderedlistdata.txt");
         Arrays.stream(strArr).forEach(s -> list.add(new MyNode<>(Integer.parseInt(s))));
+        System.out.println(list);
+        System.out.println("Enter a key to add or remove :");
         Scanner sc = new Scanner(System.in);
         Integer key = sc.nextInt();
         if (list.search(new MyNode(key)) != null)
@@ -51,13 +81,15 @@ public class DataStructureMain {
         else
             list.add(new MyNode(key));
         System.out.println(list);
-        writeDataToTextFile("orderedlistdata.txt",list);
+        writeDataToTextFile("orderedlistdata.txt", list);
     }
 
     private void unorderedListReadWriteToFile() throws CustomException {
         MyLinkedList<String> list = new MyLinkedList<>();
         String[] strArr = readDataFromFile("listdata.txt");
         Arrays.stream(strArr).forEach(s -> list.add(new MyNode(s)));
+        System.out.println(list);
+        System.out.println("Enter a key to add or remove :");
         Scanner sc = new Scanner(System.in);
         String key = sc.next();
         if (list.search(new MyNode(key)) != null)
@@ -65,7 +97,7 @@ public class DataStructureMain {
         else
             list.add(new MyNode(key));
         System.out.println(list);
-        writeDataToTextFile("listdata.txt",list);
+        writeDataToTextFile("listdata.txt", list);
     }
 
     String[] readDataFromFile(String fileName) throws CustomException {
@@ -82,7 +114,7 @@ public class DataStructureMain {
         return wordList;
     }
 
-    public <T extends Comparable<T>>void writeDataToTextFile(String fileName,MyLinkedList<T> list) throws CustomException {
+    public <T extends Comparable<T>> void writeDataToTextFile(String fileName, MyLinkedList<T> list) throws CustomException {
         Path path = Paths.get("src/main/resources/" + fileName);
         try (BufferedWriter bw = Files.newBufferedWriter(path)) {
             bw.write(list.toString().replaceAll("->", " "));
---
+++ src/main/java/org/example/linkedlist/MyStack.java
@@ -0,0 +1,34 @@
+package org.example.linkedlist;
+
+public class MyStack<T> {
+    MyLinkedList<T> myLinkedList;
+
+    public MyStack() {
+        myLinkedList = new MyLinkedList<>();
+    }
+
+    public void push(INode<T> element) {
+        myLinkedList.add(element);
+    }
+
+    @Override
+    public String toString() {
+        return myLinkedList.toString();
+    }
+
+    public INode<T> pop() {
+        return myLinkedList.popFirst();
+    }
+
+    public T peak() {
+        return myLinkedList.getHead().getKey();
+    }
+
+    public boolean isEmpty() {
+        return myLinkedList.isEmpty();
+    }
+
+    public int size() {
+        return myLinkedList.size();
+    }
+}
